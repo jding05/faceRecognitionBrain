c7afbab5bf56f70734336c5c12cc0df7a3d47cc9
jding05 <jeffding05@gmail.com>
2019-05-23 18:28:20 -0700

    connect front end to backend, with fetching

--- src/App.js
+++ src/App.js
@@ -35,11 +35,28 @@ class App extends Component {
 			imageUrl: '',
 			box: {},
 			route: 'signin',
-			isSignedIn: false
+			isSignedIn: false,
+			user: {
+				id: '',
+				name: '',
+				email: '',
+				entries: 0,
+				joined: ''
+			}
 		}
 	}
 
-calculateFaceLocation = (data) => {
+	loadUser = (data) => {
+		this.setState({user: {
+			id: data.id,
+			name: data.name,
+			email: data.email,
+			entries: data.entries,
+			joined: data.joined
+		}})
+	}
+
+	calculateFaceLocation = (data) => {
 	const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
 	const image = document.getElementById('inputimage');
 	const width = Number(image.width);
@@ -65,7 +82,22 @@ displayFaceBox = (box) => {
 		app.models.predict(
 			Clarifai.FACE_DETECT_MODEL,
 			this.state.input)
-		.then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+		.then(response => {
+			if (response) {
+				fetch('http://localhost:3000/image', {
+					method: 'put',
+					headers: {'Content-Type': 'application/json'},
+					body: JSON.stringify({
+						id: this.state.user.id
+					})
+				})
+				.then(response => response.json())
+				.then(count => {
+					this.setState(Object.assign(this.state.user, { entries: count}))
+				})
+			}
+			this.displayFaceBox(this.calculateFaceLocation(response))
+		})
 		.catch(err => console.log(err));
 	}
 
@@ -76,6 +108,7 @@ displayFaceBox = (box) => {
 			this.setState({isSignedIn: true})
 		}
 		this.setState({route: route});
+		console.log(this.state.isSignedIn, route);
 	}
 
 	render() {
@@ -87,7 +120,10 @@ displayFaceBox = (box) => {
 				{ route === 'home'
 					? <div>
 							<Logo />
-							<Rank />
+							<Rank 
+								name={this.state.user.name}
+								entries={this.state.user.entries}
+							/>
 							<ImageLinkForm
 								onInputChange={this.onInputChange}
 								onButtonSubmit={this.onButtonSubmit}
@@ -96,11 +132,9 @@ displayFaceBox = (box) => {
 						</div>
 					: (
 						route === 'signin'
-						? <Signin onRouteChange={this.onRouteChange}/>
-						: <Register onRouteChange={this.onRouteChange}/>
+						? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+						: <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
 					) 
-					
-
 				}
 			</div>
 		);
