4e56ec115ed275a4813c51e215f51058c961fb26
jding05 <jeffding05@gmail.com>
2019-05-26 16:05:39 -0700

    refactor the code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -9,11 +8,6 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
 import './App.css';
-
-
-const app = new Clarifai.App({
-	apiKey: '968166f5c1c448a3b507172081c71cf6'
-});
 
 const particlesOptions = {
 	particles: {
@@ -27,23 +21,25 @@ const particlesOptions = {
 	}
 }
 
+const initialState = {
+	input: '',
+	imageUrl: '',
+	box: {},
+	route: 'signin',
+	isSignedIn: false,
+	user: {
+		id: '',
+		name: '',
+		email: '',
+		entries: 0,
+		joined: ''
+	}
+}
+
 class App extends Component {
 	constructor() {
 		super();
-		this.state = {
-			input: '',
-			imageUrl: '',
-			box: {},
-			route: 'signin',
-			isSignedIn: false,
-			user: {
-				id: '',
-				name: '',
-				email: '',
-				entries: 0,
-				joined: ''
-			}
-		}
+		this.state = initialState;
 	}
 
 	loadUser = (data) => {
@@ -79,31 +75,37 @@ displayFaceBox = (box) => {
 
 	onButtonSubmit = () => {
 		this.setState({imageUrl: this.state.input})
-		app.models.predict(
-			Clarifai.FACE_DETECT_MODEL,
-			this.state.input)
-		.then(response => {
-			if (response) {
-				fetch('http://localhost:3000/image', {
-					method: 'put',
-					headers: {'Content-Type': 'application/json'},
-					body: JSON.stringify({
-						id: this.state.user.id
+			fetch('http://localhost:3000/imageurl', {
+				method: 'post',
+				headers: {'Content-Type': 'application/json'},
+				body: JSON.stringify({
+					input: this.state.input
+				})
+			})
+			.then(response => response.json())
+			.then(response => {
+				if (response) {
+					fetch('http://localhost:3000/image', {
+						method: 'put',
+						headers: {'Content-Type': 'application/json'},
+						body: JSON.stringify({
+							id: this.state.user.id
+						})
 					})
-				})
-				.then(response => response.json())
-				.then(count => {
-					this.setState(Object.assign(this.state.user, { entries: count}))
-				})
-			}
-			this.displayFaceBox(this.calculateFaceLocation(response))
-		})
-		.catch(err => console.log(err));
+					.then(response => response.json())
+					.then(count => {
+						this.setState(Object.assign(this.state.user, { entries: count}))
+					})
+					.catch(console.log)
+				}
+				this.displayFaceBox(this.calculateFaceLocation(response))
+			})
+			.catch(err => console.log(err));
 	}
 
 	onRouteChange = (route) => {
 		if (route === 'signout') {
-			this.setState({isSignedIn: false})
+			this.setState(initialState)
 		} else if (route === 'home') {
 			this.setState({isSignedIn: true})
 		}
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -33,20 +33,19 @@ class Register extends React.Component {
 		})
 			.then(response => response.json())
 			.then(user => {
-				if (user) {
+				if (user.id) {
 					this.props.loadUser(user)
 					this.props.onRouteChange('home');
 				}
-				console.log(user);
 			})
 	}
 
 	render() {
 		return (
 			<article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-			<main className="pa4 black-80">
-				<form className="measure">
-					<fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+				<main className="pa4 black-80">
+					<div className="measure">
+						<fieldset id="sign_up" className="ba b--transparent ph0 mh0">
 						<legend className="f1 fw6 ph0 mh0">Register</legend>
 						<div className="mt3">
 							<label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
@@ -78,18 +77,17 @@ class Register extends React.Component {
 								onChange={this.onPasswordChange}
 							/>
 						</div>
-						{/* <label className="pa0 ma0 lh-copy f6 pointer"><input type="checkbox" /> Remember me</label> */}
-					</fieldset>
-					<div className="">
+						</fieldset>
+						<div className="">
 						<input
 							onClick={this.onSubmitSignIn}
 							className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-							type="submit" 
+							type="submit"
 							value="Register"
 						/>
+						</div>
 					</div>
-				</form>
-			</main>
+				</main>
 			</article>
 		);
 	}
