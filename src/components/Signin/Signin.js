6ca1fa608bfa11deeca1089a127f4756468c4233
jding05 <jeffding05@gmail.com>
2019-05-26 17:06:14 -0700

    change the localhost path to heroku url for getting api data from server

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
 
 	// post the data to the backend
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3000/signin', {
+		fetch('https://desolate-bastion-91371.herokuapp.com/signin', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
